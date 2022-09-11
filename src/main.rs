38f6d95ee9ed5d587aa2331b322948af8363239a
Rouven Lührs <rouven.luehrs@bytabo.de>
2022-09-11 19:03:30 +0200

    implemented basic counter gui to have a starting point for guis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{button, Alignment, Button, Column, Element, Sandbox, Settings, Text};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+struct Counter {
+    value: i32,
+
+    increment_button: button::State,
+    decrement_button: button::State,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            value: 0,
+            increment_button: button::State::new(),
+            decrement_button: button::State::new(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .padding(20)
+            .align_items(Alignment::Center)
+            .push(
+                Button::new(&mut self.increment_button, Text::new("+"))
+                    .on_press(Message::IncrementPressed),
+            )
+            .push(Text::new(self.value.to_string()).size(50))
+            .push(
+                Button::new(&mut self.decrement_button, Text::new("-"))
+                    .on_press(Message::DecrementPressed),
+            )
+            .into()
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+fn main() -> iced::Result {
+    Counter::run(Settings::default())
 }
