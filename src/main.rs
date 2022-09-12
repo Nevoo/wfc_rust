22f6a5c612aa2d6060f4615d178f186e2cc51350
Rouven Lührs <rouven.luehrs@bytabo.de>
2022-09-12 22:35:42 +0200

    rendering image with sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,46 @@
 use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use std::time::Duration;
+use std::path::Path;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
+        .window("rust-sdl2 wave function collapse", 800, 600)
         .position_centered()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window
+        .into_canvas()
+        .software()
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator
+        .load_texture(Path::new("resources/road_tiles/down.png"))
+        .unwrap();
+
+    canvas.copy(&texture, None, None).unwrap();
     canvas.present();
+
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        // i = (i + 1) % 255;
-        // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        // canvas.clear();
+
+    'mainloop: loop {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
+                    keycode: Option::Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => break 'mainloop,
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
