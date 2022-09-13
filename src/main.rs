baf0561bf5fd1b2af68ba3690e4ee536344c3b8f
Rouven Lührs <rouven.luehrs@bytabo.de>
2022-09-13 09:55:23 +0200

    added imports rendering image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use sdl2::event::Event;
 use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
-use std::path::Path;
+use sdl2::rect::{Point, Rect};
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -22,14 +22,14 @@ pub fn main() {
         .unwrap();
 
     let texture_creator = canvas.texture_creator();
+
     let texture = texture_creator
-        .load_texture(Path::new("resources/road_tiles/down.png"))
+        .load_texture("resources/road_tiles/down.png")
         .unwrap();
 
-    canvas.copy(&texture, None, None).unwrap();
-    canvas.present();
-
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let rect = Rect::new(0, 0, 300, 300);
 
     'mainloop: loop {
         for event in event_pump.poll_iter() {
@@ -42,5 +42,16 @@ pub fn main() {
                 _ => {}
             }
         }
+
+        canvas.clear();
+
+        let (width, height) = canvas.output_size().unwrap();
+
+        // Treat the center of the screen as the (0, 0) coordinate
+        let screen_position = Point::new(0, 0) + Point::new(width as i32 / 2, height as i32 / 2);
+        let screen_rect = Rect::from_center(screen_position, rect.width(), rect.height());
+        canvas.copy(&texture, rect, screen_rect).unwrap();
+
+        canvas.present();
     }
 }
